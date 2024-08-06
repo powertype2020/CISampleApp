3ef3ad0f339602752cb7309918fcd8aabb23d199
naoki.takehisa <naoki.takehisa@init-inc.com>
2024-08-06 16:14:38 +0900

    ファーストコミット

---
+++ CISampleApp/CISampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CISampleAppApp.swift
+//  CISampleApp
+//
+//  Created by 武久 なおき on 2024/08/06.
+//
+
+import SwiftUI
+
+@main
+struct CISampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
